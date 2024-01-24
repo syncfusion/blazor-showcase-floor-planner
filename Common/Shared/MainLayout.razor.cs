e42a5ef9727e3391f20609a820228eaa3906749d
BalaVigneshRaviChandran <balavignesh.ravichandran@syncfusion.com>
2024-01-24 11:56:10 +0530

    849395:Floor Planner Showcase sample

---
+++ Common/Shared/MainLayout.razor.cs
@@ -0,0 +1,15 @@
+﻿using FloorPlanner;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorServerCommon_NET6.Shared
+{
+    internal class MainLayout
+    {
+
+    }
+}
