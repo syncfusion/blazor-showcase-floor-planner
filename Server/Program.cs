360375f70aec325a4616b9326336454ebe1126e3
Bharat Ram <bharatram.h@syncfusion.com>
2024-07-02 17:42:37 +0530

    891562: Add changes to only move the showcase app that's in live and then the updated README.md file's changes

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,30 +1,39 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using FloorPlanner.Components;
+using Syncfusion.Blazor;
+using FloorPlanner.Shared;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+using Syncfusion.Blazor.Popups;
+using Microsoft.Extensions.Options;
 
-namespace FloorPlanner
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddSignalR(e =>
+            {
+                e.MaximumReceiveMessageSize = 1102400000;
+            });
+builder.Services.AddScoped<SfDialogService>();
+builder.Services.AddSyncfusionBlazor();
+builder.Services.AddScoped<SampleService>(); 
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-#pragma warning disable CA1052 // Static holder types should be Static or NotInheritable
-    public class Program
-#pragma warning restore CA1052 // Static holder types should be Static or NotInheritable
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
